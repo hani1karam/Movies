749c055a779521767df0d46c031f8e7f897bcd4a
hany karam <hanykaram63@gmail.com>
2022-04-01 18:24:57 +0200

    Presnter Movies Info

---
+++ Movies/Features/MoviesInfo/MovieInfoScenePresenter.swift
@@ -0,0 +1,30 @@
+//
+//  MovieInfoScenePresenter.swift
+//  Movies
+//
+//  Created by hany karam on 3/31/22.
+//
+
+import Foundation
+class MovieInfoScenePresenter: MovieInfoScenePresentingLogic {
+    
+    // MARK: Stored Properties
+    weak var displayView: MovieInfoSceneDisplayView?
+    
+    // MARK: Initializers
+    required init(displayView: MovieInfoSceneDisplayView) {
+        self.displayView = displayView
+    }
+}
+
+extension MovieInfoScenePresenter {
+    func presentFetchedPhotos(_ response: MovieInfoScene.Fetch.Response) {
+        switch response {
+        case .success(let photos):
+            let viewModel = MovieInfoScene.Fetch.ViewModel(photos: photos)
+            displayView?.display(photos: viewModel)
+        case .failure(let error):
+            displayView?.display(error: error)
+        }
+    }
+}
