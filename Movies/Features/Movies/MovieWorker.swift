9f3180061d42cdb141b62b0f929bba6bc23bf5a2
hany karam <hanykaram63@gmail.com>
2022-04-01 18:12:54 +0200

    Presnter and Worker For Movies Feature

---
+++ Movies/Features/Movies/MovieWorker.swift
@@ -0,0 +1,26 @@
+//
+//  MovieWorker.swift
+//  Movies
+//
+//  Created by hany karam on 3/31/22.
+//
+
+import Foundation
+class MovieWorker {
+    
+    func fetchMovies(_ completionHandler: @escaping([Movie], CustomError?) -> Void) {
+        
+        if let url = Bundle.main.url(forResource: "movies", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let movies = try decoder.decode(Movies.self, from: data)
+                completionHandler(movies.movies, nil)
+            } catch let error {
+                completionHandler([], CustomError.custom(error.localizedDescription))
+            }
+        } else {
+            completionHandler([], CustomError.custom("Could not load data"))
+        }
+    }
+}
---
+++ Movies/Features/Movies/MoviesScenePresenter.swift
@@ -0,0 +1,36 @@
+//
+//  MovieInfoScenePresenter.swift
+//  Movies
+//
+//  Created by hany karam on 3/31/22.
+//
+
+import Foundation
+class MoviesScenePresenter: MoviesScenePresentingLogic {
+    weak var displayView: MoviesSceneDisplayView?
+    required init(displayView: MoviesSceneDisplayView) {
+        self.displayView = displayView
+    }
+}
+
+extension MoviesScenePresenter {
+    func presentFetchedMovies(_ response: MoviesScene.Fetch.Response) {
+        switch response {
+        case .success(let movies):
+            let viewModel = MoviesScene.ViewModel(movies: movies)
+            displayView?.display(viewModel: viewModel)
+        case .failure(let error):
+            displayView?.display(error: error)
+        }
+    }
+    
+    func presentFetchedSearchMovies(_ response: MoviesScene.Filter.Response) {
+        switch response {
+        case .success(let groupedMovies):
+            let viewModel = MoviesScene.FilteredViewModel(groupedMovies: groupedMovies)
+            displayView?.display(filteredViewModel: viewModel)
+        case .failure(let error):
+            displayView?.display(error: error)
+        }
+    }
+}
