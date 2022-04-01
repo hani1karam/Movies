54ab80362652b4124d3d06bac440b87f00686515
hany karam <hanykaram63@gmail.com>
2022-04-01 18:12:03 +0200

    Configurator For Movies Scene

---
+++ Movies/Features/Movies/MoviesSceneConfigurator.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesSceneConfigurator.swift
+//  Movies
+//
+//  Created by hany karam on 3/31/22.
+//
+
+import Foundation
+class MoviesSceneConfigurator {
+    
+    static func configure() -> MoviesVC {
+        
+        let viewController = MoviesVC()
+        let presenter = MoviesScenePresenter(displayView: viewController)
+        let interactor = MoviesSceneInteractor(presenter: presenter)
+        let router = MoviesSceneRouter(viewController: viewController, dataStore: interactor)
+        viewController.interactor = interactor
+        viewController.dataStore = interactor
+        viewController.router = router
+        return viewController
+    }
+    
+}
