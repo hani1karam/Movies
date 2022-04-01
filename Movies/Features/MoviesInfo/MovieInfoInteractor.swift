24ec9eb677f4095460c78a8e8fb05cf501073b49
hany karam <hanykaram63@gmail.com>
2022-04-01 18:23:43 +0200

    Details Info Interactor

---
+++ Movies/Features/MoviesInfo/MovieInfoInteractor.swift
@@ -0,0 +1,40 @@
+//
+//  MovieInfoInteractor.swift
+//  Movies
+//
+//  Created by hany karam on 3/31/22.
+//
+
+class MovieInfoSceneInteractor: MovieInfoSceneBusinessLogic, MovieInfoSceneDataStore {
+    
+    // MARK: Stored Properties
+    let presenter: MovieInfoScenePresentingLogic
+    var worker = MovieInfoWorker()
+    
+    var photos: [Photo] = []
+    
+    var movie = Movie()
+    
+    // MARK: Initializers
+    required init(presenter: MovieInfoScenePresentingLogic) {
+        self.presenter = presenter
+    }
+}
+
+extension MovieInfoSceneInteractor {
+    
+    func fetchMoviePhotos(title: String) {
+        worker.fetchMoviePhotos(title: title) { [weak self] (photos, error) in
+            
+            guard let self = self else { return }
+            
+            if let error = error {
+                let response = MovieInfoScene.Fetch.Response.failure(error)
+                self.presenter.presentFetchedPhotos(response)
+            } else {
+                let response = MovieInfoScene.Fetch.Response.success(photos)
+                self.presenter.presentFetchedPhotos(response)
+            }
+        }
+    }
+}
